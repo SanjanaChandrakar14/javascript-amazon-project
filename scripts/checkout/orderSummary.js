152fa4181e11ed77361f0a740ebd61cdea686231
SanjanaChandrakar14 <sanjana84314@gmail.com>
2025-08-02 18:10:33 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,14 +5,16 @@ import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';//us
 
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js"; //default export
 
-import { deliveryOptions,getDeliveryOption } from "../../data/deliveryOptions.js"
+import { deliveryOptions,getDeliveryOption } from "../../data/deliveryOptions.js";
+
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
-hello();
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-// console.log(deliveryDate);
-console.log(deliveryDate.format('dddd, MMMM D'));
+// hello();
+// const today = dayjs();
+// const deliveryDate = today.add(7, 'days');
+// // console.log(deliveryDate);
+// console.log(deliveryDate.format('dddd, MMMM D'));
 
 
 export function renderOrderSummary(){
@@ -144,6 +146,8 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
 
     container.remove();
+
+    renderPaymentSummary();
   });
 
 });
@@ -153,6 +157,7 @@ document.querySelectorAll('.js-delivery-option').forEach((element) => {
     const { productId, deliveryOptionId } = element.dataset;
     updateDeliveryOption(productId, deliveryOptionId);
     renderOrderSummary();
+    renderPaymentSummary();
   })
 })
 
